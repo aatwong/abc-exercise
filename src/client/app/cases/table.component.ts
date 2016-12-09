a77393eb58b1d826c6aca97ab71b061099adca09
Andrew Atwong <andrew.atwong@accenture.com>
2016-12-09 11:19:58 -0800

    formatted cases component

--- src/client/app/cases/table.component.ts
+++ src/client/app/cases/table.component.ts
@@ -1,24 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Case } from './case';
 import { CaseService } from './case.service';
+//import { FilterPipe } from './filter-pipe';
+import { Pipe, PipeTransform } from '@angular/core';
 
-// const CASES: Case[] = [
-//   {patientId: 1234, dob: '01/01/1900', name: 'Scott Walker', serviceId: 4455, service:'calHEERS'},
-//   {patientId: 1234, dob: '01/01/1901', name: 'Linda Walker', serviceId: 4455, service:'calHEERS'},
-//   {patientId: 1234, dob: '01/01/1910', name: 'Caitlin Walker', serviceId: 4455, service:'calHEERS'},
-//   {patientId: 1233, dob: "06/23/1999", name: 'adam jonesss', serviceId: 1124, service: "obamacare"}
-// ];
 
 @Component({
   moduleId: module.id,
   selector: 'table-component',
+  //pipes: [FilterPipe],
   providers: [CaseService],
   templateUrl: 'table.component.html',
   styleUrls: ['table.component.css']
 })
 
 export class TableComponent implements OnInit {
-  //cases = CASES.filter(c => c.service === "obamacare");
+  
   public cases: Case[];
 
   constructor(private _caseService: CaseService) {}  //"dependency injection"
@@ -32,4 +29,5 @@ export class TableComponent implements OnInit {
   ngOnInit(): any {  //when component is done loading
     this.getCases();
   }
+
 }
